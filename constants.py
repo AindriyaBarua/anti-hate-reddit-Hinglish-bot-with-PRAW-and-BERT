25c128ac336668f4ba334b5a77d27653a205ea9e
Barua,Aindriya <aindriya.barua@cerner.com>
2021-10-15 19:49:05 +0530

    added ban feature

--- constants.py
+++ constants.py
@@ -1,9 +1,16 @@
+"""
+Developed by Aindriya Barua in October, 2021
+"""
+
 CLIENT_ID = "**************"
 CLIENT_SECRET = "**************"
 USERNAME = "**************"
 PASSWORD = "**************"
 USER_AGENT = "AntiHateIndia by u/**************"
 
-TARGET_SUB = "indianhatespeechtest"
+TARGET_SUB = "************"
 
 HATE_DETECTION_MODEL = 'model.pkl'
+
+WARNING =  "AntiHateIndia Bot says: WARNING! You have been detected using hate speech! You shall be banned with no further explanation after three such detections. Please be respectful!"
+BAN_REASON = "Banned for using hate speech after three warnings"
