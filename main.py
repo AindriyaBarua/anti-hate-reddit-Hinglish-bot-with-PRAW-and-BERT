3c98ca5616d54198c247859cac163c7747975e5b
Aindriya Barua <barua.aindriya@gmail.com>
2021-10-15 08:42:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ reddit = praw.Reddit(client_id=client_id,
 target_sub = "indianhatespeechtest"
 subreddit = reddit.subreddit(target_sub)
 
-# phrase to trigger the bot
-#trigger_phrase = "! NoMoreHate"
 
 # enchant dictionary
 filename = 'model.pkl'
@@ -30,8 +28,7 @@ model = pickle.load(open(filename, 'rb'))
 # check every comment in the subreddit
 for comment in subreddit.stream.comments():
     #comment.reply("Ye bot comment hai")
-    prediction, raw_output = model.predict([comment.body])
-    if(prediction == 1):
-        comment.reply("@" + comment.author + "Your comment is deleted as it is detected as hate speech. Please respect your fellow users.")
-        comment.delete
-
+    if "Anti Hate Bot says:" not in comment.body:
+      prediction, raw_output = model.predict([comment.body])
+      if(prediction == 1):
+          comment.reply("Anti Hate Bot says: This is hate speech. Please be respectful here!")
